315b0b3a4cce99dfc1fda1560d1d8caaff5e0f47
ThomasMoellerR <thomas.moeller.r@gmail.com>
2019-02-18 18:59:45 +0100

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,21 @@
+void setup() {
+
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+    
+  delay(3000);   
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait for a second
+
+
+
+
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
